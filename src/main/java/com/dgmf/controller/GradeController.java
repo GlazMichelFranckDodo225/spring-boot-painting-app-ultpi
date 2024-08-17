414d7aa029101efbd1e8e8cd86345346ddaaa8eb
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-17 20:28:54 +0200

    "/grades" Endpoint

---
+++ src/main/java/com/dgmf/controller/GradeController.java
@@ -0,0 +1,13 @@
+package com.dgmf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class GradeController {
+    @GetMapping("/grades")
+    public String getGrades() {
+        return "grades";
+    }
+}
